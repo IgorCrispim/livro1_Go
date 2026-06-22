5a865179f8ae1e06a5ef758b84ef5b1ab6336884
Igor Crispim <igorcrispim1812@gmail.com>
2026-06-22 11:50:01 -0300

    fim do cap 3 - melhor organizacao de codigo

---
+++ cmd/web/routes.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"net/http"
+)
+
+func (app *application) routes() *http.ServeMux {
+	mux := http.NewServeMux()
+
+	fileServe := http.FileServer(http.Dir("./ui/static"))
+	mux.Handle("GET /static/", http.StripPrefix("/static", fileServe))
+
+	mux.HandleFunc("GET /{$}", app.home)
+	mux.HandleFunc("GET /snippet/view/{id}", app.snippetView)
+	mux.HandleFunc("GET /snippet/create", app.snippetCreate)
+
+	mux.HandleFunc("POST /snippet/create", app.snippetCreatePost)
+
+	return mux
+}
