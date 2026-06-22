e37f1c89dd8480d92fd0c3dff45ee7bd2db978c1
Igor Crispim <igorcrispim1812@gmail.com>
2026-06-22 09:16:36 -0300

    adicao de flag de porta que sera inicializado o server 3.2

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,20 +1,27 @@
 package main
 
 import (
+	"flag"
 	"log"
 	"net/http"
 )
 
 func main() {
+	// Define a new command-line flag with the name 'addr', a default vaue of ":4000"
+	// and some short help text explaning what the flag controls. The value os the
+	// flag will be stored in the addr variable at runtime.
+	addr := flag.String("addr", ":4000", "HTTP network address")
+
+	// Importantly, we use the flag.Parse() function to parse the comand-line flag.
+	// This reads in the command-line flag value and assgns it to the addr
+	// variable. You need to call this *before* you use the addr variable
+	// otherwise it will always contain the default value of ":4000". If any errors are
+	// encountered during parsing the application will be terminated.
+	flag.Parse()
+
 	mux := http.NewServeMux()
 
-	// Create a file server which serves files out of the "./ui/static" directory.
-	// Note that the path given to the http.Dir function is relative to the project
-	// directory root.
 	fileServer := http.FileServer(http.Dir("./ui/static/"))
-	// Use the mux.Handle() function to register the file server as the handler
-	// all URL paths that start with "/static/". For matching paths, we strip the
-	// "/static" prefix before the request reaches the file server.
 	mux.Handle("GET /static/", http.StripPrefix("/static", fileServer))
 
 	mux.HandleFunc("GET /{$}", home)
@@ -22,8 +29,14 @@ func main() {
 	mux.HandleFunc("GET /snippet/create", snippetCreate)
 	mux.HandleFunc("POST /snippet/create", snippetCreatePost)
 
-	log.Print("stating server on :4000")
+	// The value returned from the flag.String() function is a pointer to the flag
+	// value, not the value itself. So in this code, that means the addr variable
+	// is actually a pointer, and we need to deference it (i.e. prefix it with
+	// the * symbol) before using it. Note tha we're using the log.Printf()
+	// function to interpolate the address with the log message.
+	log.Print("stating server on %s", *addr)
 
-	err := http.ListenAndServe(":4000", mux)
+	// And we pass the deferenced addr pointer to http.ListenAndServe() too.
+	err := http.ListenAndServe(*addr, mux)
 	log.Fatal(err)
 }
